cb825094a1022a98371b856f0ed4839d0137a458
Eduardo-Luiz-Campos <campos.santos.eduardo@escola.pr.gov.br>
2026-03-28 13:16:44 -0300

    Trabalho finalizado, adicionado todos os métodos e rotas

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = 3001;
-const fs = require('fs');
+const produtosRoutes = require('./routes/produtos');
 
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // Rota para enviar texto no diretório principal
     res.send('Servidor da API de estoque está rodando com sucesso!');
 });
 
-function lerProdutos(){
-    const dados = fs.readFileSync('produtos.json', 'utf-8');
+app.use('/produtos', produtosRoutes);
 
-    return JSON.parse(dados);
-};
-
-app.get('/produtos', (req, res) => {
-    const produtos = lerProdutos();
-    res.JSON(produtos);
-});
-
-app.post('/produtos', (req, res) => {
-
-    const { nome, descricao, preco, quantidade, categoria } = req.body;
-    const produtos = lerProdutos();
-    const novo = { id: produtos.length+1, nome, descricao, preco, quantidade, categoria };
-
-    produtos.push(novo);
-
-    fs.writeFileSync('produtos.json', JSON.stringify(produtos, null, 2));
-
-    res.status(201).json(novo);
-});
-
-app.listen(PORT, () => {
+app.listen(PORT, () => { // Para mostrar aonda o servidor está rodando no terminal e executar mais fácil
     console.log(`Servidor rodando na porta http://localhost:${PORT}`);
 });
